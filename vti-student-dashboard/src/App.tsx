956c4c3afed09f97a59d0fdb8be00b042c005ea3
2neiuh9-Cirr <duc28071999@gmail.com>
2026-09-24 13:34:26 +0700

    Set StudentPage as default

--- vti-student-dashboard/src/App.tsx
+++ vti-student-dashboard/src/App.tsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SettingPage from "./pages/SettingPage";
 import CoursePage from "./pages/CoursePage";
 import StudentPage from "./pages/StudentPage";
-import HomePage from "./pages/HomePage";
+//import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       {/* Your app content */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<StudentPage />} />
         <Route path="/setting" element={<SettingPage />} />
         <Route path="/course" element={<CoursePage />} />
         <Route path="/student" element={<StudentPage />} />
